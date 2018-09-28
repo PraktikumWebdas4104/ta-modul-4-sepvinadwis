d7d49b6cdd265955da47f499865680f99753e812
sepvinadwis <43513131+sepvinadwis@users.noreply.github.com>
2018-09-28 23:47:41 +0700

    Update formawal.php
    SEPVINA DWI SANTOSA
    6701174066
    D3MI-41-04

--- formawal.php
+++ formawal.php
@@ -1,5 +1,22 @@
-<?php
+<h1>FORM LOGIN</h1>
+    <form action="proses.php" method="post">  
+        USERNAME = <input type="text" name="username"><br/><br/>
+        PASSWORD = <input type="password" name="password"><br/><br/>
+        <br/>
+        <input type="submit" name="login" value="LOGIN">
+    </form>
+<?php 
+    if (isset($_POST['login'])) {
+      $useer=$_POST['username'];
+      $pass=$_POST['password'];
 
+      if ($useer=="Sepvina"&&$pass=="1998") {
+        echo "LOGIN BERHASIL!!!";
+?>
 
-
+<?php 
+    }else{
+      echo "LOGIN GAGAL";
+    }
+  }
 ?>
